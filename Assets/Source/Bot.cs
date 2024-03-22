f668a3c8b6eaa5d64204c22e78a4da77c6fdb7f0
Alex <yevsyukovaleks@gmail.com>
2024-03-22 03:36:51 +0300

    add project

---
+++ Assets/Source/Bot.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bot : MonoBehaviour
+{
+    public float speed;// Множитель скорости
+    public float rotation;// Множитель поворота
+    public LayerMask raycastMask;// Маска для сенсоров
+
+    private float[] input = new float[5];// Входные данные для нейронной сети
+    public NeuralNetwork network;
+
+    public int position;// Номер чекпоинта на трассе
+    public bool collided;// Показывает, столкнулась ли машина с препятствием
+
+    void FixedUpdate()// FixedUpdate вызывается с постоянным интервалом
+    {
+        if (!collided)// Если машина не столкнулась со стеной, она использует нейронную сеть для получения выходных данных
+        {
+            for (int i = 0; i < 5; i++)// Рисует пять отладочных лучей как входные данные
+            {
+                Vector3 newVector = Quaternion.AngleAxis(i * 45 - 90, new Vector3(0, 1, 0)) * transform.right;// Вычисляет угол луча
+                RaycastHit hit;
+                Ray Ray = new Ray(transform.position, newVector);
+
+                if (Physics.Raycast(Ray, out hit, 10, raycastMask))
+                {
+                    input[i] = (10 - hit.distance) / 10;// Возвращает расстояние, где 1 - близко
+                }
+                else
+                {
+                    input[i] = 0;// Если ничего не обнаружено, возвращает 0 в сеть
+                }
+            }
+
+            float[] output = network.FeedForward(input);// Вызов сети для прямого распространения
+
+            transform.Rotate(0, output[0] * rotation, 0, Space.World);// Управляет движением машины
+            transform.position += this.transform.right * output[1] * speed;// Управляет поворотом машины
+        }
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if(collision.collider.gameObject.layer == LayerMask.NameToLayer("CheckPoint"))// Проверяет, прошла ли машина ворота
+        {
+            GameObject[] checkPoints = GameObject.FindGameObjectsWithTag("CheckPoint");
+            for (int i=0; i < checkPoints.Length; i++)
+            {
+                if(collision.collider.gameObject == checkPoints[i] && i == (position + 1 + checkPoints.Length) % checkPoints.Length)
+                {
+                    position++;// Если ворота впереди, увеличивает позицию, которая используется для оценки эффективности сети
+                    break;
+                }
+            }
+        }
+        else if(collision.collider.gameObject.layer != LayerMask.NameToLayer("Learner"))
+        {
+            collided = true;// Остановить работу, если машина столкнулась
+        }
+    }
+
+    public void UpdateFitness()
+    {
+        network.fitness = position;// Обновляет эффективность сети для сортировки
+    }
+}
---
+++ Assets/Source/NeuralNetwork.cs
@@ -0,0 +1,217 @@
+﻿using System.Collections.Generic;
+using System;
+using System.IO;
+
+public class NeuralNetwork : IComparable<NeuralNetwork>
+{
+    private int[] layers;// Слои
+    private float[][] neurons;// Нейроны
+    private float[][] biases;// Смещения
+    private float[][][] weights;// Веса
+    private int[] activations;// Активации
+
+    public float fitness = 0;// Эффективность
+
+    public NeuralNetwork(int[] layers)
+    {
+        this.layers = new int[layers.Length];
+        for (int i = 0; i < layers.Length; i++)
+        {
+            this.layers[i] = layers[i];
+        }
+        InitNeurons();
+        InitBiases();
+        InitWeights();
+    }
+
+    private void InitNeurons()// Создает пустой массив для хранения нейронов в сети
+    {
+        List<float[]> neuronsList = new List<float[]>();
+        for (int i = 0; i < layers.Length; i++)
+        {
+            neuronsList.Add(new float[layers[i]]);
+        }
+        neurons = neuronsList.ToArray();
+    }
+
+    private void InitBiases()// Инициализирует и заполняет массив для смещений в сети
+    {
+        List<float[]> biasList = new List<float[]>();
+        for (int i = 0; i < layers.Length; i++)
+        {
+            float[] bias = new float[layers[i]];
+            for (int j = 0; j < layers[i]; j++)
+            {
+                bias[j] = UnityEngine.Random.Range(-0.5f, 0.5f);
+            }
+            biasList.Add(bias);
+        }
+        biases = biasList.ToArray();
+    }
+
+    private void InitWeights()// Инициализирует случайный массив для весов в сети
+    {
+        List<float[][]> weightsList = new List<float[][]>();
+        for (int i = 1; i < layers.Length; i++)
+        {
+            List<float[]> layerWeightsList = new List<float[]>();
+            int neuronsInPreviousLayer = layers[i - 1];
+            for (int j = 0; j < neurons[i].Length; j++)
+            {
+                float[] neuronWeights = new float[neuronsInPreviousLayer];
+                for (int k = 0; k < neuronsInPreviousLayer; k++)
+                {
+                    neuronWeights[k] = UnityEngine.Random.Range(-0.5f, 0.5f);
+                }
+                layerWeightsList.Add(neuronWeights);
+            }
+            weightsList.Add(layerWeightsList.ToArray());
+        }
+        weights = weightsList.ToArray();
+    }
+
+    public float[] FeedForward(float[] inputs)// Прямой проход, входы >==> выходы
+    {
+        for (int i = 0; i < inputs.Length; i++)
+        {
+            neurons[0][i] = inputs[i];
+        }
+        for (int i = 1; i < layers.Length; i++)
+        {
+            int layer = i - 1;
+            for (int j = 0; j < neurons[i].Length; j++)
+            {
+                float value = 0f;
+                for (int k = 0; k < neurons[i - 1].Length; k++)
+                {
+                    value += weights[i - 1][j][k] * neurons[i - 1][k];
+                }
+                neurons[i][j] = activate(value + biases[i][j]);
+            }
+        }
+        return neurons[neurons.Length - 1];
+    }
+
+    public float activate(float value)
+    {
+        return (float)Math.Tanh(value);
+    }
+
+    public void Mutate(int chance, float val)// Используется в качестве простой функции мутации для любых генетических реализаций
+    {
+        for (int i = 0; i < biases.Length; i++)
+        {
+            for (int j = 0; j < biases[i].Length; j++)
+            {
+                biases[i][j] = (UnityEngine.Random.Range(0f, chance) <= 5) ? biases[i][j] += UnityEngine.Random.Range(-val, val) : biases[i][j];
+            }
+        }
+
+        for (int i = 0; i < weights.Length; i++)
+        {
+            for (int j = 0; j < weights[i].Length; j++)
+            {
+                for (int k = 0; k < weights[i][j].Length; k++)
+                {
+                    weights[i][j][k] = (UnityEngine.Random.Range(0f, chance) <= 5) ? weights[i][j][k] += UnityEngine.Random.Range(-val, val) : weights[i][j][k];
+                }
+            }
+        }
+    }
+
+    public int CompareTo(NeuralNetwork other) // Сравнение производительности нейронных сетей
+    {
+        if (other == null) return 1;
+
+        if (fitness > other.fitness)
+            return 1;
+        else if (fitness < other.fitness)
+            return -1;
+        else
+            return 0;
+    }
+
+    public NeuralNetwork copy(NeuralNetwork nn) // Для создания глубокой копии, чтобы гарантировать сериализацию массивов
+    {
+        for (int i = 0; i < biases.Length; i++)
+        {
+            for (int j = 0; j < biases[i].Length; j++)
+            {
+                nn.biases[i][j] = biases[i][j];
+            }
+        }
+        for (int i = 0; i < weights.Length; i++)
+        {
+            for (int j = 0; j < weights[i].Length; j++)
+            {
+                for (int k = 0; k < weights[i][j].Length; k++)
+                {
+                    nn.weights[i][j][k] = weights[i][j][k];
+                }
+            }
+        }
+        return nn;
+    }
+
+    public void Load(string path)// Загружает смещения и веса из файла в нейронную сеть
+    {
+        TextReader tr = new StreamReader(path);
+        int NumberOfLines = (int)new FileInfo(path).Length;
+        string[] ListLines = new string[NumberOfLines];
+        int index = 1;
+        for (int i = 1; i < NumberOfLines; i++)
+        {
+            ListLines[i] = tr.ReadLine();
+        }
+        tr.Close();
+        if (new FileInfo(path).Length > 0)
+        {
+            for (int i = 0; i < biases.Length; i++)
+            {
+                for (int j = 0; j < biases[i].Length; j++)
+                {
+                    biases[i][j] = float.Parse(ListLines[index]);
+                    index++;
+                }
+            }
+
+            for (int i = 0; i < weights.Length; i++)
+            {
+                for (int j = 0; j < weights[i].Length; j++)
+                {
+                    for (int k = 0; k < weights[i][j].Length; k++)
+                    {
+                        weights[i][j][k] = float.Parse(ListLines[index]);
+                        index++;
+                    }
+                }
+            }
+        }
+    }
+
+    public void Save(string path)// Используется для сохранения смещений и весов в сети в файл
+    {
+        File.Create(path).Close();
+        StreamWriter writer = new StreamWriter(path, true);
+
+        for (int i = 0; i < biases.Length; i++)
+        {
+            for (int j = 0; j < biases[i].Length; j++)
+            {
+                writer.WriteLine(biases[i][j]);
+            }
+        }
+
+        for (int i = 0; i < weights.Length; i++)
+        {
+            for (int j = 0; j < weights[i].Length; j++)
+            {
+                for (int k = 0; k < weights[i][j].Length; k++)
+                {
+                    writer.WriteLine(weights[i][j][k]);
+                }
+            }
+        }
+        writer.Close();
+    }
+}
